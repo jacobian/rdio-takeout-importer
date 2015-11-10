fe6800687bcdecf9e954e5a80daf1fa77830e789
Jannis Leidel <jannis@leidel.info>
2015-11-10 21:23:20 +0100

    Minor cosmetic fixes.

--- r2s.py
+++ r2s.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 import base64
 import click
-import collections
-import csv
 import pathlib
 import spotipy
 import spotipy.util
 import sqlite3
-import sys
 import time
 import urllib
 from more_itertools import chunked
@@ -22,6 +19,7 @@ REDIRECT_URI = 'FIXME'
 class SkipAlbum(Exception):
     pass
 
+
 def main():
     spotify = connect_to_spotify()
 
@@ -30,12 +28,12 @@ def main():
     if not collection_db_file.exists():
         click.secho("collection.sqlite3 doesn't exist, run rdio_export_to_sqlite3.py first", fg='red')
         return
-    
+
     db = sqlite3.connect(str(collection_db_file))
     cursor = db.cursor()
-    cursor.execute('''SELECT artist, album 
-                      FROM collection 
-                      WHERE skip = 0 
+    cursor.execute('''SELECT artist, album
+                      FROM collection
+                      WHERE skip = 0
                       AND complete = 0
                       AND track_count > 5
                       ORDER BY track_count DESC''')
@@ -66,13 +64,15 @@ def main():
 
     db.close()
 
+
 def connect_to_spotify():
-    token = spotipy.util.prompt_for_user_token(USERNAME, 
-        scope = 'user-library-modify', 
-        client_id = CLIENT_ID,
-        client_secret = CLIENT_SECRET,
-        redirect_uri = REDIRECT_URI)
+    token = spotipy.util.prompt_for_user_token(USERNAME,
+        scope='user-library-modify',
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=REDIRECT_URI)
     return spotipy.Spotify(auth=token)
+
 
 def print_header(artist, album, current, total):
     click.clear()
@@ -83,12 +83,13 @@ def print_header(artist, album, current, total):
     header_template = u'=== {title} {pad} {progress} ==='
     click.secho(
         header_template.format(
-            title = title,
-            pad = '='*pad_width,
-            progress = progress,
-        ), 
+            title=title,
+            pad='=' * pad_width,
+            progress=progress,
+        ),
         bg='blue', fg='white'
     )
+
 
 def handle_not_found(db, artist, album, prompt=True):
     if prompt:
@@ -102,12 +103,13 @@ def handle_not_found(db, artist, album, prompt=True):
                           WHERE artist = ? AND album = ?''', [artist, album])
         db.commit()
 
+
 def select_album(original_artist, original_album, albums, current_position, total_number):
     for album in albums:
         print_header(original_artist, original_album, current_position, total_number)
         click.secho(u'\nFound: "{0}"\n'.format(album['name']), fg='green')
         print imgcat(album['images'][0]['url'])
-        
+
         action = None
         while action not in ('y', 'n', 's'):
             actions = '/'.join(under_first(s) for s in ('yes', 'next', 'skip'))
@@ -120,6 +122,7 @@ def select_album(original_artist, original_album, albums, current_position, tota
             raise SkipAlbum()
         else:
             continue
+
 
 def add_to_spotify(db, spotify, album, original_artist, original_album):
     album = spotify.album(album['uri'])
@@ -140,19 +143,22 @@ def add_to_spotify(db, spotify, album, original_artist, original_album):
 
     cursor = db.cursor()
     cursor.execute('''UPDATE collection SET complete = 1
-                      WHERE artist = ? AND album = ?''', 
+                      WHERE artist = ? AND album = ?''',
                    [original_artist, original_album])
     db.commit()
     click.secho('Done ', fg='green', nl=False)
     time.sleep(0.25)
+
 
 def imgcat(url):
     content = base64.encodestring(urllib.urlopen(url).read())
     text = "\033]1337;File=inline=1;size={0};px:{1}\a"
     return text.format(str(len(content)), content)
 
+
 def under_first(s):
     return click.style(s[0], underline=True) + s[1:]
+
 
 if __name__ == '__main__':
     try:
